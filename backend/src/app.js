8878338646fc38be804ce98d8f79440770dfdec8
Srutidev Barman <srutidevbarman2002@gmail.com>
2026-04-05 23:50:29 +0530

    added path

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,12 @@ import authRoutes from "./routes/authRoutes.js";
 import itemRouter from "./routes/itemRoutes.js";
 import cors from "cors";
 import graphRouter from "./routes/graphRoutes.js";
+import path from "path";
 const app = express();
 
 // Body parser middleware
 app.use(express.json());
-
+app.use(express.static("/public"));
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,5 +25,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api", itemRouter);
 app.use("/api/graph", graphRouter);
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
 
 export default app;
